3e89e6d34dc36719948184d453b3cd8402a7cbca
vladimirperov85 <vova.perov.85@gmail.com>
2026-07-07 15:30:53 +0300

    добавлено отображение главной страницы в глобальный urls.

--- core/urls.py
+++ core/urls.py
@@ -18,8 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from ads import views
 urlpatterns = [
+    path('', views.home_view, name='home'),
     path('admin/', admin.site.urls),
     path('ads/', include('ads.urls', namespace='ads')),
 ]
